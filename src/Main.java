bb322b1950d415baffbe825d752bf30634be2289
Ethan Murray <ethan.nm.509@gmail.com>
2026-02-06 16:15:39 -0500

    commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     void main(){
+        Scanner scanner = new Scanner(System.in);
 
+        double shippingRate = 0.02;
+        double cost;
+        double newCost;
+
+        System.out.println("Input the cost of your item:");
+
+        if(scanner.hasNextDouble()){
+            cost = scanner.nextDouble();
+            scanner.nextLine();
+
+            if(cost >= 100){
+                //Apply shipping
+                newCost = cost * shippingRate + cost;
+                System.out.println("Your new cost with shipping is " + newCost);
+            }else{
+                //Don't apply shipping
+                System.out.println("Free shipping! Your item costs " + cost);
+            }
+        }else{
+            String b = scanner.nextLine();
+            System.out.println("\nBad input try again: "+b);
+        }
+
+        scanner.close();
     }
 }
